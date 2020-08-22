88e7b7ce4c1b053b0da7b53c21ce8d72e5309b2d
Tamas Jozsa <tjozsa@gmail.com>
2020-08-22 20:27:01 +0200

    Implemented substraction operator and onEqual

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,6 +49,36 @@ class MainActivity : AppCompatActivity() {
             false
         } else {
             value.contains("/") || value.contains("*") || value.contains("-") || value.contains("+")
+        }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric) {
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+
+                    var firstValue = splitValue[0]
+                    var secondValue = splitValue[1]
+                    
+                    if (!prefix.isEmpty()){
+                        firstValue = prefix + firstValue
+                    }
+
+                    tvInput.text = (firstValue.toDouble() - secondValue.toDouble()).toString()
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+
         }
     }
 }
