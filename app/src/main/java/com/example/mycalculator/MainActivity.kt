c91d40f1adddb0144c9d4d530d4f5fb91f788573
Tamas Jozsa <tjozsa@gmail.com>
2020-08-22 19:57:14 +0200

    Challenge 2 START: implement onClear function when CLR button is pressed.

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onDigit(view: View) {}
+    fun onDigit(view: View) {
+        tvInput.append((view as Button).text)
+    }
+
+    // Challenge 2: START implement onClear function when CLR button is pressed.
 }
